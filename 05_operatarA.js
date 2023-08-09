5311f5ac32c9b69edc5feb7337deacc14dd9ca02
Amar8983 <amarlaxmikantmaurya@gmail.com>
2023-08-09 17:47:49 +0530

    Create 05_operatarA.js

---
+++ 05_operatarA.js
@@ -0,0 +1,41 @@
+var squreOfWordLength = function(char){
+    var result = char.length;
+    console.log(`Length of word: ${result} `);
+}
+squreOfWordLength('java script');
+console.log('squre of word is : 110');
+squreOfWordLength('Google Chrome');
+console.log('squre of word is:169');
+squreOfWordLength('Developer Smart');
+console.log('squre of word is 225');
+console.log('=================================================================================');
+var greet = "I am Angular Developer";
+
+var lengthGreet= greet.length;
+
+console.log(greet,'Length of String is:', lengthGreet);
+var result = greet.split(' ');
+var words = result.length;
+console.log(`total no. of words given string is : ${words}`);
+
+
+var result = 22 / 4;
+console.log(`Total no. of word length divided divided by word count : ${result}`);
+var num1 = 22*4 ;
+console.log(`Total no of word length multiply by word count : ${num1}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
