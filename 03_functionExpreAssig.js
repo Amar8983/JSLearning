105a5d7431aab62a7fd9706bb484f15eab411a94
Amar8983 <amarlaxmikantmaurya@gmail.com>
2023-08-09 17:29:58 +0530

    Create 03_functionExpreAssig.js

---
+++ 03_functionExpreAssig.js
@@ -0,0 +1,70 @@
+var squre = function(num){
+    var result = num*num;
+    console.log(`Squre of 5 is: ${result} `);
+}
+squre(5);
+squre(6);
+squre(25);
+squre(100);
+squre(67.89);
+squre(59);
+console.log('=================================================================');
+console.log(typeof squre );
+console.log('===================================================================');
+var areaOfRactangle = function(num){
+    var result = num*num;
+    console.log(`Area of reactangle plot : ${result} `);
+    
+
+}
+areaOfRactangle(917,499);
+console.log('==========================================================================');
+var value1 = "Mahi";
+var value2 = "Raina";
+console.log('Before Swap',"value1:",value1,"value2:",value2);
+var temp = value1;
+value1 = value2;
+value2 = temp;
+console.log('After Swap',"value1:",value1,"value2:",value2);
+console.log('=================================================================================');
+var value1 = "55";
+var value2 = "77";
+console.log('Before Swap',"value1:",value1,"value2:",value2);
+var temp = value1;
+value1 = value2;
+value2 = temp;
+console.log('After Swap',"value1:",value1,"value2:",value2);
+console.log('===================================================================================');
+var greet = "JS the most popular language of internet";
+
+var lengthGreet= greet.length;
+
+console.log(greet,'Length of String is:', lengthGreet);
+var charAt7 = greet.charAt(7);
+
+console.log(`${greet} - Char at 7th index is ${charAt7}`);
+var charAt12 = greet.charAt(12);
+
+console.log(`${greet} - Char at 12th index is ${charAt12}`);
+var charAtLastIndex = greet.charAt(lengthGreet-1);
+
+console.log(` Last Char of given string is : ${charAtLastIndex}`);
+var charAt0 = greet.charAt(0);
+
+console.log(`${greet} - First char of given string is : ${charAt0}`);
+var charAt37 = greet.charAt(37);
+
+console.log(`${greet} - Third last Character of given string is: ${charAt37}`);
+
+
+
+
+
+
+
+ 
+
+
+
+
+
