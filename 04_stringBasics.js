05183cb51d177504190fe14f119af989c4634acb
Amar8983 <amarlaxmikantmaurya@gmail.com>
2023-08-09 12:59:28 +0530

    Create 04_stringBasics.js

---
+++ 04_stringBasics.js
@@ -0,0 +1,15 @@
+console.log("=============Assignment1===========================");
+function stringBasics(){
+    console.log('My Dream Company Name is - TCS');
+}
+stringBasics();
+
+console.log('===================Assignment2=====================');
+var hobby1 = 'playing Badminton,';
+var hobby2 = 'Watching Movies,';
+var hobby3 = 'Reading Books';
+console.log('my hobbies are:',hobby1,hobby2,hobby3);
+var result = 'playing Badminton Watching Movies Reading Books';
+var lengthGreet = result.length;
+console.log('length of string is:',lengthGreet );
+
