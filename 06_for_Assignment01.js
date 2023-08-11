612940c5a45a6fc60c0ac9b3cc3b8cc6fa77ce7b
Amar8983 <amarlaxmikantmaurya@gmail.com>
2023-08-12 00:52:54 +0530

    Create 06_for_Assignment01.js

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,34 @@
+for (let index = 5; index <= 15 ; index++){
+    console.log(index);   
+}
+console.log("=============================================");
+for (let index = 50; index >= 40; index--){
+    console.log(index);   
+}
+console.log("=============================================");
+console.log(`first 15 odd numbers`);
+for (let index = 1; index <= 15 ; index=index+2){
+    console.log(index);   
+}
+console.log("=============================================");
+console.log(`first 10 even numbers`);
+for (let index = 0; index <= 10 ; index=index+2){
+    console.log(index);   
+}
+console.log("=====================================");
+
+console.log(`print table of 5 like - 5 10 15 20...50`);
+for (let index = 5; index <= 50; index=index+5) {
+    console.log(index);
+    
+}
+console.log(`print table of 10 like - 10 20 30 ....100`);
+for (let index = 10; index <= 100; index=index+10) {
+    console.log(index);
+    
+}
+console.log(`print table of 10 reverse order - 100 90 80....10`);
+for (let index = 100; index >= 10; index=index-10) {
+    console.log(index);
+    
+}
