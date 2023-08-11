cd963d98aa12b8fcf9b073233b7313743d45a1ea
Amar8983 <amarlaxmikantmaurya@gmail.com>
2023-08-12 00:33:01 +0530

    Create 06_ifElseHandsOn.js

---
+++ 06_ifElseHandsOn.js
@@ -0,0 +1,64 @@
+var num = 45;
+
+if (num%2==0) {
+
+    console.log(`${num} is even`);
+
+} else {
+
+    console.log(`${num} is odd`);
+
+}
+
+var num = 70;
+
+if (num%2==0) {
+
+    console.log(`${num} is even`);
+
+} else {
+
+    console.log(`${num} is odd`);
+
+}
+var num = 67;
+
+if (num%2==0) {
+
+    console.log(`${num} is even`);
+
+} else {
+
+    console.log(`${num} is odd`);
+
+}
+var num = 98;
+
+if (num%2==0) {
+
+    console.log(`${num} is even`);
+
+} else {
+
+    console.log(`${num} is odd`);
+
+}
+
+var java = "javaScript-ES6"
+if ( java.length>=10) {
+
+    console.log(`${java} given string is greater than 10`);
+    
+} else {
+    console.log(`${java} given string is not greater than 10`);
+    
+}
+
+var language = "javascript Language";
+var result = language.includes("java")
+console.log(`string satrts with cahracter :${result}`);
+
+
+
+
+
