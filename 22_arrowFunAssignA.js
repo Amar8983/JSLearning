f9416ed6f8f1b277eefea219eb8051955de9f488
Amar8983 <amarlaxmikantmaurya@gmail.com>
2023-08-23 02:44:25 +0530

    Create 22_arrowFunAssignA.js

---
+++ 22_arrowFunAssignA.js
@@ -0,0 +1,17 @@
+
+let greet = (arg1) => {
+    console.log(`${arg1}`);
+}
+greet("Good Morning,Today is Tuesday")
+
+let multiply =(arg2,arg3,arg4)=>{
+    console.log(`Multiplication of given value : ${arg2*arg3*arg4}`);
+}
+multiply(5,5,2)
+multiply(10,4,1)
+
+let addition =(arg5,arg6,arg7,arg8,arg9)=>{
+    console.log(`Addtion of given value :${arg5+arg6+arg7+arg8+arg9}`);
+}
+addition(100,100,200,349,756)
+addition("I AM","learning","ES6","features","in depth")
